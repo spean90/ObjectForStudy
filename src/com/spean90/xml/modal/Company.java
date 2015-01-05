766c23c099bcc0471dd2c2c1db72752d13ebf595
spean90 <spean90@gmail.com>
2015-01-05 16:03:54 +0800

    使用dom完成xml解析；并且封装成对象

---
+++ src/com/spean90/xml/modal/Company.java
@@ -0,0 +1,18 @@
+package com.spean90.xml.modal;
+
+import java.util.List;
+
+public class Company {
+	
+	private List<Department> departments;
+
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
+	
+}
+
---
+++ src/com/spean90/xml/modal/Department.java
@@ -0,0 +1,25 @@
+package com.spean90.xml.modal;
+
+public class Department {
+	private String name;
+	private String id;
+	private String manager;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getManager() {
+		return manager;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+}
