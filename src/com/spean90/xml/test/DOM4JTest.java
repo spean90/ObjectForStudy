bc203333884d168764eb7f77ab4fb20357bb4bdb
spean90 <spean90@gmail.com>
2015-01-06 17:43:59 +0800

    完成DOM4J根据对象创建xml

--- src/com/spean90/xml/test/DOM4JTest.java
+++ src/com/spean90/xml/test/DOM4JTest.java
@@ -1,13 +1,19 @@
 package com.spean90.xml.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
 import com.spean90.xml.modal.Company;
 import com.spean90.xml.modal.Department;
@@ -15,18 +21,63 @@ import com.spean90.xml.modal.Department;
 public class DOM4JTest {
 
 	public static void main(String[] args) {
+		DOM4JTest dom4jTest = new DOM4JTest();
 		SAXReader saxReader = new SAXReader();
 		try {
 			Document document = saxReader.read("cfg/company.xml");
-			Company company = parseDocument(document);
+			Company company = dom4jTest.parseDocument(document);
 			System.out.println(company.getDepartments().size());
 			System.out.println(company.getDepartments().get(0).getName());
-		} catch (DocumentException e) {
+			dom4jTest.createXml(company);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static  Company parseDocument(Document document) {
+	public void createXml(Company company) {
+		Document document = DocumentHelper.createDocument();
+		Element element = document.addElement("company");
+		List<Department> departments = company.getDepartments();
+		for (Department department : departments) {
+			Element element_department = element.addElement("department");
+			//添加节点属性
+			element_department.addAttribute("id", department.getId());
+			//添加子节点、并设置文本内容
+			element_department.addElement("name").setText(department.getName());
+			element_department.addElement("manager").setText(department.getManager());
+		}
+		
+		
+		
+		
+		//生成文件
+		//设置格式
+		OutputFormat format = OutputFormat.createPrettyPrint();
+		//设置xml的encoding属性；
+		//format.setEncoding("GBK");
+		File f = new File("cfg/company_dom4j.xml");
+		FileOutputStream fout = null;
+		XMLWriter writer = null;
+		try {
+			fout = new FileOutputStream(f);
+			writer = new XMLWriter(fout,format);
+			//设置不转义；默认转义
+			writer.setEscapeText(false);
+			writer.write(document);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				fout.close();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public Company parseDocument(Document document) {
 		Company company = new Company();
 		List<Department> departments = new ArrayList<Department>();
 		company.setDepartments(departments);
