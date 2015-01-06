511472f12c62ab5d00cd7e7000e679eaed264902
spean90 <spean90@gmail.com>
2015-01-06 11:22:20 +0800

    使用JDOM完成对象封�

--- src/com/spean90/xml/JDOMTest.java
+++ src/com/spean90/xml/JDOMTest.java
@@ -1,11 +1,15 @@
 package com.spean90.xml;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
+
+import com.spean90.xml.modal.Company;
+import com.spean90.xml.modal.Department;
 
 public class JDOMTest {
 	public static void main(String[] args) {
@@ -13,20 +17,41 @@ public class JDOMTest {
 		Document document = null;
 		try {
 			document = saxBuilder.build("cfg/company.xml");
-			Element root = document.getRootElement();
-			List<Element> children = root.getChildren();
-			System.out.println(children.size());
-			for (Element element : children) {
-				//获取节点的属性
-				System.out.println(element.getAttributeValue("id"));
-				//获取节点的子节点的节点名和值
-				List<Element>nodes = element.getChildren();
-				for (Element node : nodes) {
-					System.out.println(node.getName()+" : "+node.getValue());
-				}
-			}
+			Company company = parseDocument(document);
+			System.out.println(">>>>>>>>>>>>>>>");
+			System.out.println(company.getDepartments().size());
+			System.out.println(company.getDepartments().get(0).getManager());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static  Company parseDocument(Document document) {
+		Company company = new Company();
+		List<Department> departments = new ArrayList<Department>();
+		company.setDepartments(departments);
+		Element root = document.getRootElement();
+		List<Element> children = root.getChildren();
+		System.out.println(children.size());
+		for (Element element : children) {
+			Department department = new Department();
+			//获取节点的属性
+			System.out.println(element.getAttributeValue("id"));
+			department.setId(element.getAttributeValue("id"));
+			//获取节点的子节点的节点名和值
+			List<Element>nodes = element.getChildren();
+			for (Element node : nodes) {
+				System.out.println(node.getName()+" : "+node.getValue());
+				if(node.getName().equals("name")) {
+					department.setName(node.getValue());
+				}
+				if(node.getName().equals("manager")) {
+					department.setManager(node.getValue());
+				}
+			}
+			departments.add(department);
+		}
+		return company;
+	}
+	
 }
