f760da3144427c57156336d951ed3a4ee7585939
spean90 <spean90@gmail.com>
2015-01-08 16:54:06 +0800

    添加logback支持；

--- src/com/spean90/hibernate4/test/DaoTest.java
+++ src/com/spean90/hibernate4/test/DaoTest.java
@@ -3,16 +3,20 @@ package com.spean90.hibernate4.test;
 import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.spean90.hibernate4.dao.UserInfoDao;
 import com.spean90.hibernate4.modal.UserInfo;
 
 public class DaoTest {
 
+	Logger logger = LoggerFactory.getLogger(DaoTest.class);
 	
 	
 	//@Test
 	public void testUserInfoDaoList() {
+		logger.info("in...testDao...");
 		UserInfoDao userInfoDao = new UserInfoDao();
 		List<UserInfo> list = userInfoDao.getUserInfoList();
 		System.out.println(list.size());
@@ -20,9 +24,11 @@ public class DaoTest {
 	
 	@Test
 	public void testSaveUserInfo() {
+		logger.info("in...testDao...");
 		UserInfoDao userInfoDao = new UserInfoDao();
 		UserInfo userInfo = new UserInfo();
 		userInfo.setAccount("abc");
 		System.out.println(userInfoDao.saveUserInfo(userInfo));
+		
 	}
 }
